2bb821a75938cedebba1885638b9105d6d08aedf
aryfairuzyan07-hue <aryfairuzyan07@gmail.com>
2026-06-09 23:44:05 +0800

    Update server.js

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const PORT = process.env.PORT || 3000;
 
 app.use(cors());
 app.use(express.json());
-app.use(express.static(path.join(__dirname, 'frontend')));
+app.use(express.static(__dirname));
 
 // Koneksi ke MySQL Railway (variabel lingkungan otomatis)
 const db = mysql.createPool({
